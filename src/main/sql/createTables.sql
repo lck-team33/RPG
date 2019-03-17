40d831edd6cb6a6d8a7a9d7bb55e2ce9e24ce4da
andi <akk-pg@team33.de>
2019-03-17 19:02:12 +0100

    updated createTables.sql

--- src/main/sql/createTables.sql
+++ src/main/sql/createTables.sql
@@ -1,7 +1,19 @@
 use lenasrpg01;
 
 CREATE TABLE characters (
-    id INT NOT NULL AUTO_INCREMENT,
-    creation TIMESTAMP NOT NULL,
-    CONSTRAINT pk_characters PRIMARY KEY (id)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ts_creation BIGINT NOT NULL
 );
+
+CREATE TABLE character_properties (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    character_id INT NOT NULL FOREIGN KEY REFERENCES characters(id),
+    property VARCHAR(255) NOT NULL,
+    content VARCHAR(255),
+    ts_creation BIGINT NOT NULL
+);
+
+CREATE INDEX character_properties_property
+    ON character_properties (property);
+CREATE INDEX character_properties_content
+    ON character_properties (content);
